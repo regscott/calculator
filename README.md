25de7abb790ad69d62df1fdca961724ac221f1f7
Regina Scott <scott.g.regina@gmail.com>
2022-05-23 01:43:39 -0400

    Add README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Calculator
+Calculator is a command line program that supports two-operand addition, subtraction, multiplication, and division.
+
+## User Stories 
+- [x] User can select an operation to perform
+- [x] User can input two operands
+- [x] User sees the result of their operation
+- [ ] User can input more than two operands
+- [ ] User can use parentheses to indicate order of operations 
+- [ ] User previous answer is saved so they can reuse it as operand
--- calculator.py
+++ calculator.py
@@ -1,16 +1,16 @@
 type_of_operation =int(input("Which operation:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n"))
 if type_of_operation > 0 and type_of_operation < 5:
-    first_operator = int(input("First operator: ")) 
-    second_operator = int(input("Second operator: "))
+    first_operand = int(input("First operator: ")) 
+    second_operand = int(input("Second operator: "))
 result = 0
 if type_of_operation == 1:
-    result = first_operator + second_operator   
+    result = first_operand + second_operand   
 elif type_of_operation == 2:
-    result = first_operator - second_operator   
+    result = first_operand - second_operand   
 elif type_of_operation == 3:
-    result = first_operator * second_operator   
+    result = first_operand * second_operand   
 elif type_of_operation == 4:
-    result = first_operator / second_operator
+    result = first_operand / second_operand
 if type_of_operation > 0 and type_of_operation < 5:
     print("Result: " + str(result))
 else: 
