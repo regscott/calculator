e00855807dc3df5e62988906b6f73501efff4cc4
Regina Scott <scott.g.regina@gmail.com>
2022-05-22 22:57:31 -0400

    Handle invalid user input

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 type_of_operation =int(input("Which operation:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n"))
-first_operator = int(input("First operator: ")) 
-second_operator = int(input("Second operator: "))
+if type_of_operation > 0 and type_of_operation < 5:
+    first_operator = int(input("First operator: ")) 
+    second_operator = int(input("Second operator: "))
 result = 0
 if type_of_operation == 1:
     result = first_operator + second_operator   
@@ -10,4 +11,7 @@ elif type_of_operation == 3:
     result = first_operator * second_operator   
 elif type_of_operation == 4:
     result = first_operator / second_operator
-print("Result: " + str(result))  
+if type_of_operation > 0 and type_of_operation < 5:
+    print("Result: " + str(result))
+else: 
+    print("Invaild input")
